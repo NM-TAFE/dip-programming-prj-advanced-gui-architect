98df849bd894a89a63028832060cd420155bbf30
Cedric <yacmov@gmail.com>
2024-06-02 22:35:16 +0800

    feat: search bar

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -1,9 +1,8 @@
 const inputElement = document.getElementById('search-in');
-const searchButton = document.getElementById("search-button");
 const searchResults = document.querySelectorAll(".continue-items");
 
 
-searchButton.addEventListener('click', () => {
+inputElement.addEventListener('input', () => {
      const query = inputElement.value.trim().toLowerCase();
      searchResults.forEach((item) => {
          // Do something with the item, e.g., log its text content
